6ffd458462d680d3a769d4c574f2bac4ead4e2bb
RocioPomaSilvestre <pomarocio05@gmail.com>
2023-09-27 17:29:09 -0400

    27-09

--- backend/routes/proyecto_ciudad_o_comunidad.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const connection = require('../connection');
-const router = express.Router();
-var auth = require('../services/authentication');
-
-
-module.exports = router;
