a240e16fae4c13f9155ef1bbee7bc2e3cc429fef
RocioPomaSilvestre <pomarocio05@gmail.com>
2023-09-28 15:22:54 -0400

    28/09/2023

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    // production: true,
-    // apiUrl: 'https://pdcg-api.dev.404.codes'
-    production: false,
-    apiUrl: 'http://localhost:4000'
+    production: true,
+    apiUrl: 'https://pdcg-api.dev.404.codes'
+    // production: false,
+    // apiUrl: 'http://localhost:4000'
     
 };
